8e514e607192c648910fbe416e60a40783d0e0dc
Carletto <John_Carletto@PremierInc.com>
2020-05-30 16:36:34 -0400

    add QOL stuff

--- examples/hidraw_controller_map.js
+++ examples/hidraw_controller_map.js
@@ -2,12 +2,16 @@ var GameController = require('../gamecontroller.js');
 var HID = require('node-hid');
 const Vendors = require('../lib/vendors.js');
 
-console.log(HID.devices());
-
 var devs = GameController.getDevices();
+console.log("Devices found : " + devs);
+
 if (devs.length > 0) {
-    var path = GameController.getDevicePath(Vendors[devs[0]]);
-	console.log(path);
+	
+	var ven = Vendors[dev[0]];
+	console.log("Choosing Device : " + ven);
+	
+    var path = GameController.getDevicePath(ven);
+	console.log("Path : " + path);
     var hid = new HID.HID(path);
 
     hid.on("data", function (data) {
