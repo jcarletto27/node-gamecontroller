038f7a5737cf576dec75fc0f100db87d4176476f
Carletto <John_Carletto@PremierInc.com>
2020-05-30 16:37:11 -0400

    add QOL stuff

--- examples/hidraw_controller_map.js
+++ examples/hidraw_controller_map.js
@@ -7,7 +7,7 @@ console.log("Devices found : " + devs);
 
 if (devs.length > 0) {
 	
-	var ven = Vendors[dev[0]];
+	var ven = Vendors[devs[0]];
 	console.log("Choosing Device : " + ven);
 	
     var path = GameController.getDevicePath(ven);
