38aa94a5cf9a1434460b1815f866179020cf2b63
Carletto <John_Carletto@PremierInc.com>
2020-05-30 14:37:30 -0400

    add an example to list by path

--- examples/list_path.js
+++ examples/list_path.js
@@ -2,7 +2,7 @@ var GameController = require('../gamecontroller.js');
 
 var dev = GameController.getDevices();
 console.log(dev);
-for( let i =0; i < dev.length i++){
+for( let i =0; i < dev.length; i++){
 var path = GameController.getDevicePath(dev[i]);
 console.log(path);
 }
