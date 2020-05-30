a9b6d790e3ac1c0bdd416258689963ca0776aefa
Carletto <John_Carletto@PremierInc.com>
2020-05-30 16:32:00 -0400

    add QOL stuff

--- examples/hidraw_controller_map.js
+++ examples/hidraw_controller_map.js
@@ -7,7 +7,7 @@ console.log(HID.devices());
 var devs = GameController.getDevices();
 if (devs.length > 0) {
     var path = GameController.getDevicePath(Vendors[devs[0]]);
-
+	console.log(path);
     var hid = new HID.HID(path);
 
     hid.on("data", function (data) {
